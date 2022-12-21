595d1be0b32b6c86918fbbb7b2a94327752d49d7
DaKinker <tiesvandenbroek05@gmail.com>
2022-12-21 22:12:56 +0100

    hahaah

--- GamblerScript.js
+++ GamblerScript.js
@@ -1,4 +1,9 @@
-function test()
-{
-	alert("hoi");
-}
+const fs = require('fs');
+
+fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
