c49152c072bae86bf5d4611893273672fee6915a
DaKinker <tiesvandenbroek05@gmail.com>
2022-12-21 22:36:49 +0100

    Update GamblerScript.js

--- GamblerScript.js
+++ GamblerScript.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+fs.readFile("CSV\LiveMonthLiveAction.csv", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
